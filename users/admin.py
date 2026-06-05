d4ff625dbd73351487708741c7d3401ae76e088d
Leonardo Kapone <nairp6502@gmail.com>
2026-06-05 11:41:22 +0200

    Added option to user: set up a warehouse and limited view

--- users/admin.py
+++ users/admin.py
@@ -5,20 +5,31 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    # 1. Добавляем 'warehouse' в колонки таблицы списка пользователей
     list_display = (
         'id',
         'username',
         'email',
         'role',
+        'warehouse',
         'is_staff',
         'is_superuser',
         'last_login',
     )
 
-    list_filter = ('role', 'is_staff', 'is_superuser')
+
+    list_filter = ('role', 'warehouse', 'is_staff', 'is_superuser')
+
 
     fieldsets = UserAdmin.fieldsets + (
-        ('Дополнительно', {'fields': ('role', 'created_at', 'updated_at')}),
+        ('Дополнительно', {
+            'fields': (
+                'role',
+                'warehouse',
+                'created_at',
+                'updated_at'
+            )
+        }),
     )
 
     readonly_fields = ('created_at', 'updated_at', 'last_login')
